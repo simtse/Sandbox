e5aebdba7e057b648c53f9acfca5a76a718eda1d
Simon Tse <stse@slack-corp.com>
2021-05-19 23:34:30 -0700

    initial commit

---
+++ app/src/main/java/simon/sandbox/MainActivity.kt
@@ -0,0 +1,38 @@
+package simon.sandbox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import simon.sandbox.ui.theme.SandboxTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      SandboxTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  SandboxTheme {
+    Greeting("Android")
+  }
+}
