db422340080317c3e53ef9911f255c24b2ace83a
Simon Tse <stse@slack-corp.com>
2021-05-28 16:56:07 -0700

    Animating the background of the text on click step 7 end

--- app/src/main/java/simon/sandbox/CodeLabComposeBasics.kt
+++ app/src/main/java/simon/sandbox/CodeLabComposeBasics.kt
@@ -3,6 +3,9 @@ package simon.sandbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
@@ -13,8 +16,10 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,7 +59,16 @@ fun MyScreenContent(names: List<String> = List(1000) { "Hello Android $it" }) {
 
 @Composable
 fun Greeting(name: String) {
-  Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+  var isSelected by remember { mutableStateOf(false) }
+  val backgroundColor by
+    animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+
+  Text(
+    text = "Hello $name!",
+    modifier = Modifier
+      .padding(24.dp)
+      .background(color = backgroundColor)
+      .clickable { isSelected = !isSelected })
 }
 
 @Composable
